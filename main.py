63fe50080b9f219bc4de1c75ae913b607c6c15d0
SimonsSoscka <sabinahus0609@gmail.com>
2025-03-06 23:28:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ async def main():
     dp = Dispatcher()
     
     await bot.send_message(chat_id="917911613", text="Бот запущен!")
-    await dp.start_polling(bot)
+from aiogram import Bot, Dispatcher
+import asyncio
+
+TOKEN = "7462721993:AAHaWBxQymZv19uBsSsS6ws9Cum13C1djrQ"  # Подставь свой токен бота
+bot = Bot(token=TOKEN)
+dp = Dispatcher()    
+await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
